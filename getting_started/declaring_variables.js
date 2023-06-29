6687dfd5ed9e8b053cf80145755063825ddd5411
LucksDesperation15 <50502281+LucksDesperation15@users.noreply.github.com>
2023-06-29 17:16:06 -0400

    variable practice (#1)

---
+++ getting_started/declaring_variables.js
@@ -0,0 +1,12 @@
+var petDog = "Rex";
+var petCat = "Pepper";
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is: ", petDog);
+console.log("My pet cat's name is: ", petCat);
+var catSound = "purr";
+var dogSound = "woof";
+console.log(petDog, "says", dogSound);
+console.log(petCat, "says", catSound);
+var catSound2 = "meow";
+console.log(petCat, "now says", catSound2);
