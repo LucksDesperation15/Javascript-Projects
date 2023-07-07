191349faa49df32adcb61edb4407baa7e9e33089
brian <bpcostello15@gmail.com>
2023-07-07 14:29:50 -0400

    finish counter app

--- passenger_counter_app/index.js
+++ passenger_counter_app/index.js
@@ -1,12 +1,18 @@
 let count = 0;
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el")
 
 function add() {
     count ++;
-    document.getElementById("count-el").innerText = count
+    countEl.textContent = count
     console.log(`The count is ${count}`);
 }
 
 function save() {
+    let saveAndCount = `${count}, `;
+    saveEl.textContent += saveAndCount
     alert(`The count is saved at ${count}`);
-    // console.log(`The count is saved at ${count}`);
+    count = 0
+    countEl.textContent = count
+    console.log(`The count is saved at ${count}`);
 }
