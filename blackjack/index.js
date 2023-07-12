1dde51b9b4930b163d03ff792d6bdd4a83eb876c
brian <bpcostello15@gmail.com>
2023-07-12 17:07:37 -0400

    adding new functionality

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,15 +1,42 @@
 let firstCard = 9
-let secondCard = 11
+let secondCard = 12
+let cards = [firstCard, secondCard]
+let newCard
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-console.log(sum)
+function startGame() {
+    renderGame()
+}
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?")
+function renderGame() {
+
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    }
+    else if (sum === 21) {
+        message = "Blackjack! You won!"
+        hasBlackJack = true
+    }
+    else {
+        message = "You busted!"
+        isAlive = false
+    }
+    
+    cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`
+    sumEl.textContent = `Sum: ${sum}`
+    messageEl.textContent = message
+
 }
-else if (sum === 21) {
-    console.log("You won!")
-}
-else {
-    console.log("You busted!")
+
+function getNewCard() {
+    newCard = 2
+    cards.push(newCard)
+    sum += newCard
+    renderGame()
 }
