6ff930db3126d5b250d8e648e9279f7930799c6c
LucksDesperation15 <50502281+LucksDesperation15@users.noreply.github.com>
2023-07-20 17:25:19 -0400

    Mobile app (#16)
    * finish counter app
    
    * initial commit
    
    * adding first functionality
    
    * adding firebase

--- mobile_app/index.js
+++ mobile_app/index.js
@@ -1,8 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    database_url : "https://playground-99818-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, "movies")
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    
+    push(moviesInDB, inputValue)
     
     console.log(inputValue)
 <<<<<<< HEAD
